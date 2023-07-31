de7e3db1a5d035d185a46b60a3ce08fdf3ae39da
Yasmeen Abdellatif <112444332+Yasmin3bdellatif@users.noreply.github.com>
2023-07-31 16:36:18 +0200

    first commit

---
+++ classes/calc.class.php
@@ -0,0 +1,44 @@
+<?php
+
+    class Calc {
+
+        public $opertator;
+        public $num1;
+        public $num2;
+
+
+        public function __construct(string $opertator, int $one, int $two) {
+            $this->opertator = $opertator;
+            $this->num1 = $one;
+            $this->num2 = $two;
+
+        }
+
+        public function calculator(){
+            switch( $this->opertator) {
+                case 'add' :
+                    $result = $this->num1 + $this->num2;
+                    return $result;
+                    break;
+                case 'sub' :
+                    $result = $this->num1 - $this->num2;
+                    return $result;
+                    break;
+                case 'div' :
+                    $result = $this->num1 / $this->num2;
+                    return $result;
+                    break;
+                case 'mul' :
+                    $result = $this->num1 * $this->num2;
+                    return $result;
+                    break;
+                default :
+                    echo "error!"
+                    break;
+            }
+        }
+    }
+
+    
+
+?>
